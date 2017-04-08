e1e75befbaed2acfc81f6d4de3d35db5d450bcac
danja <danny.ayers@gmail.com>
2017-04-08 23:15:34 +0200

    starting playing with tensorflow & keras

---
+++ cat/conv-cat.py
@@ -0,0 +1,228 @@
+# modified version of https://github.com/erikreppel/visualizing_cnns/blob/master/visualize_cnns.ipynb
+# to run from shell with python3
+
+from keras.layers import Convolution2D, MaxPooling2D, Activation
+from keras.models import Sequential
+
+
+import numpy as np
+import matplotlib
+matplotlib.use("TKAgg")
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import cv2  # only used for loading the image, you can use anything that returns the image as a np.ndarray
+
+cat = cv2.imread('baz.png')
+# cat = mpimg.imread('phil.png')
+# print(cat)
+#
+plt.imshow(cat)
+plt.show(cat)
+
+print(cat.shape)
+
+# a model with 1 Convolutional layer
+
+model = Sequential()
+model.add(Convolution2D(3,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+
+# Keras expects batches of images, so we have to add a dimension to trick it into being nice
+cat_batch = np.expand_dims(cat,axis=0)
+
+### DANNY
+# For a multi-class classification problem
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+              
+conv_cat = model.predict(cat_batch)
+
+# here we get rid of that added dimension and plot the image
+def visualize_cat(model, cat):
+    # Keras expects batches of images, so we have to add a dimension to trick it into being nice
+    cat_batch = np.expand_dims(cat,axis=0)
+    conv_cat = model.predict(cat_batch)
+    conv_cat = np.squeeze(conv_cat, axis=0)
+    print(conv_cat.shape)
+    plt.imshow(conv_cat)
+
+visualize_cat(model, cat)
+
+
+# 10x10 Kernel ConvCat
+
+model = Sequential()
+model.add(Convolution2D(3,    # number of filter layers
+                        10,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        10,    # x dimension of kernel
+                        input_shape=cat.shape))
+visualize_cat(model, cat)
+
+# Cat with 1 filter
+
+# Note: matplot lib is pretty inconsistent with how it plots these weird cat arrays.
+# Try running them a couple of times if the output doesn't quite match the blog post results.
+def nice_cat_printer(model, cat):
+    '''prints the cat as a 2d array'''
+    cat_batch = np.expand_dims(cat,axis=0)
+    conv_cat2 = model.predict(cat_batch)
+
+    conv_cat2 = np.squeeze(conv_cat2, axis=0)
+    print(conv_cat2.shape)
+    conv_cat2 = conv_cat2.reshape(conv_cat2.shape[:2])
+
+    print(conv_cat2.shape)
+    plt.imshow(conv_cat2)
+
+model = Sequential()
+model.add(Convolution2D(1,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+
+# Keras expects batches of images, so we have to add a dimension to trick it into being nice
+nice_cat_printer(model, cat)
+
+# 15x15 kernel size
+model = Sequential()
+model.add(Convolution2D(1,    # number of filter layers
+                        15,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        15,    # x dimension of kernel
+                        input_shape=cat.shape))
+
+nice_cat_printer(model, cat)
+
+# add a relu activation
+
+model = Sequential()
+model.add(Convolution2D(1,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets add a new activation layer!
+model.add(Activation('relu'))
+
+nice_cat_printer(model, cat)
+
+model = Sequential()
+model.add(Convolution2D(3,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets add a new activation layer!
+model.add(Activation('relu'))
+
+visualize_cat(model, cat)
+
+
+# Max Pooling
+
+model = Sequential()
+model.add(Convolution2D(1,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets add a new max pooling layer!
+model.add(MaxPooling2D(pool_size=(5,5)))
+
+nice_cat_printer(model, cat)
+
+model = Sequential()
+model.add(Convolution2D(3,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets add a new max pooling layer!
+model.add(MaxPooling2D(pool_size=(5,5)))
+
+# nice_cat_printer(model, cat)
+visualize_cat(model, cat)
+
+
+model = Sequential()
+model.add(Convolution2D(1,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets activate then pool!
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(5,5)))
+
+nice_cat_printer(model, cat)
+
+
+model = Sequential()
+model.add(Convolution2D(3,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets activate then pool!
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(5,5)))
+
+visualize_cat(model, cat)
+
+
+# Cat after the convolutional and pooling stages of LeNet
+
+# 1 filter in each conv layer for pretty printing
+model = Sequential()
+model.add(Convolution2D(1,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets activate then pool!
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Convolution2D(1,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets activate then pool!
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+
+nice_cat_printer(model, cat)
+
+
+# 3 filters in conv1, then 1 filter for pretty printing
+model = Sequential()
+model.add(Convolution2D(3,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets activate then pool!
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Convolution2D(1,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets activate then pool!
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+nice_cat_printer(model, cat)
+
+# 3 filters in both conv layers
+model = Sequential()
+model.add(Convolution2D(3,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets activate then pool!
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(3,3)))
+model.add(Convolution2D(3,    # number of filter layers
+                        3,    # y dimension of kernel (we're going for a 3x3 kernel)
+                        3,    # x dimension of kernel
+                        input_shape=cat.shape))
+# Lets activate then pool!
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+visualize_cat(model, cat)
