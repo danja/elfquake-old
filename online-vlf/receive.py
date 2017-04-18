a7e4cbfea3eec212bdbd7acf41a5cb9ba5f1e47f
danja <danny.ayers@gmail.com>
2017-04-18 13:22:38 +0200

    ogg parser getting there

--- online-vlf/receive.py
+++ online-vlf/receive.py
@@ -6,9 +6,9 @@ host = "78.46.38.217"
 port = 80
 path = "/vlf15"
 
-chunk_size = 10000
+chunk_size = 100
 # sync_magic = bytes("OggS", 'utf-8')
-sync_re = re.compile(b"OggS") # magic string between Ogg pages
+
 # url = "78.46.38.217:80/vlf15"
 connection = http.client.HTTPConnection(host, port)
 connection.request("GET", path)
@@ -20,6 +20,8 @@ print(response.status, response.reason)
 connection.request("GET", path)
 response = connection.getresponse()
 
+sync_re = re.compile(b"OggS") # magic string between Ogg pages
+
 init_state = 0 # before the first OggS marker
 accumulating_state = 1
 end_state = 2
@@ -28,22 +30,69 @@ state = init_state
 page = bytes(0)
 chunks = bytes(0)
 
+class OggPage:
+    def __init__(self):
+        self.data = bytes(0) # empty bytes
+
+    #    if self.sync_re.match(page_start)
+#        self.page_start = page_start
+
+
+    def append(self, chunk):
+        self.data = self.data + chunk
+
+    def parse(self):
+        self.page_type = self.page[5]
+#  The value 1 specifies that the page contains data of a packet continued from the previous page. The value 2 specifies that this is the first page of the stream, and the value 4 specifies that this is the last page of the stream. These values can be combined with addition or logical OR.
+        self.granule_position = self.page[6:14]
+        self.serial_number = self.page[7:18]
+        self.sequence_number = self.page[19:25]
+        self.checksum = self.page[23:27]
+        self.n_segments = self.page[28]
+
+    def __str__(self):
+        return str(self.data)
+
+def handle_page(page):
+    print("PAGE")
+    print(len(page.data))
+    print(page)
+
 # print(response.read(10000), end='', flush=True) # .decode("ISO-8859-1")
+current_page = OggPage()
+current_data = bytes(0)
 
 while not response.closed:
-    chunks = chunks + response.read(chunk_size)
-    search = sync_re.search(chunks)
-    print(search.start())
-    if search:
-        if init_state:
-            state = start_state
-            continue
-        if accumulating_state:
-            page = chunks[0 : search.start()-1]
-            print("PAGE")
-            print(page) # .decode("ISO-8859-1")
-            page = chunks[search.end()+1 : len(chunks)-1]
-            chunks = bytes(0)
+    chunk = response.read(chunk_size)
+    chunks = chunks + chunk
+    search = sync_re.search(chunks) # check if there is an Oggs marker
+    if search == None:
+        continue
+
+    # add tail end of previous
+    current_page.append(chunks[0:search.start()-1])
+    handle_page(current_page)
+    current_page = OggPage()
+    print("search.end() = "+str(search.end()))
+    print("len(chunks) = "+str(len(chunks)))
+    chunks = chunks[:search.end()]
+    print("len(chunks) = "+str(len(chunks)))
+
+
+
+    #
+    # if search:
+    #     if init_state:
+    #         page = chunks[0 : search.start()-1]
+    #         state = start_state
+    #         continue
+    #     if accumulating_state:
+    #         print("PAGE")
+    #         print(page) # .decode("ISO-8859-1")
+    #         page = chunks[search.end()+1 : len(chunks)-1]
+    #         chunks = bytes(0)
+
+
 
 
     #while state != end_state:
